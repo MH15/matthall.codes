1c13ecf12dc3955732b575ef3a368d7eae200384
Matt Hall <hall.2586@osu.edu>
2020-07-01 16:22:55 -0400

    yer

--- nconfig.js
+++ nconfig.js
@@ -8,7 +8,7 @@ module.exports = {
     // The `meta` object is sent to every template.
     meta: {
         title: "Site Title",
-        description: "This blog is statically awesome!",
+        description: "This is the homepage of Matt Hall, computer science student at the Ohio State University in Columbus, Ohio. I post about computer graphics, software engineering, and other things.",
         author: "Matt Hall",
     },
     // All authors must be defined here.
